d2019833f66402956a67f874719a5183162f0ae2
Sara Gryka <sarakgryka@hotmail.com>
2019-09-23 14:07:58 -0700

    buttons and api

---
+++ gif.js
@@ -0,0 +1,163 @@
+$(document).ready(function () {
+
+    //Create an array for buttons//
+
+    let topic = ["Detroit Lions", "Pittsburg Steelers", "Chicago Bears", "Denver Broncos", "Oakland Raiders", "Dallas Cowboys", "Miami Dolphins", "New England Patriots", "Cleveland Browns", "Tennesee Titans", "Green Bay Packers", "Jacksonville Jaguars"];
+
+
+    // define variables//query, api//
+    let apiKey = "api_key=trDYfnaBbGQV9hOlzSN32F9lysZX9d7o";
+
+    let searchTopic;
+
+    let queryUrl = `https://api.giphy.com/v1/gifs/search?${apiKey}&q=${searchTopic}&limit=10&lang=en`;
+
+    let state = "still";
+
+    //dynamically create buttons//loop//
+
+    function start() {
+
+        for (i = 0; i < topic.length; i++) {
+
+            let buttons = $("<button>");
+            buttons.addClass("btn btn-danger");
+            buttons.text(topic[i]);
+            buttons.attr("data-value", topic[i]);
+            $(".buttonsDiv").append(buttons);
+
+          
+
+        }
+    };
+
+    start();
+
+    //onclick funciton to return results of button clicked//this data attribute//
+
+    $(".btn").on("click", function () {
+
+
+        //searchInfo();
+
+
+
+        //});
+
+
+        //pulls information from api and returns on page//still animation and rating//
+
+    searchTopic = $(this).attr("data-value");
+    console.log(searchTopic);
+
+     queryUrl = `https://api.giphy.com/v1/gifs/search?${apiKey}&q=${searchTopic}&limit=10&lang=en`;
+
+
+        $.ajax({
+
+            url: queryUrl,
+            method: "GET"
+        })
+            .then(function (response) {
+
+                console.log(response);
+
+
+
+                for (i = 0; i < response.data.length; i++) {
+
+                    let imageUrl = response.data[i].images.fixed_height_still.url;
+
+                    let allResults = $("<div>");
+                    allResults.addClass("allResults");
+
+
+                    let apiResults = $("<img>");
+                    apiResults.attr("src", imageUrl);
+                    
+
+                    let ratings =$ ("<h1>");
+                    ratings.text("rating: " + response.data[i].rating);
+
+                    $(".allResults").append(ratings);
+                    $(".allResults").append(apiResults);
+                   
+                    $(".resultsDiv").append(allResults);
+                    console.log(queryUrl);
+
+                    //  console.log(response.data[i].images.fixed_height_still);
+
+
+
+
+                };
+
+
+            },
+
+                function (error) {
+
+                    console.log("error");
+
+
+
+                });
+
+    });
+
+
+
+
+
+
+
+    //onclick function for animation and still//
+
+
+
+    // form function for search option to get value and push to the array//
+
+
+    //function calll that takes each topic in the array and remakes the buttons//
+
+    //function to not allow double search or empty search//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
